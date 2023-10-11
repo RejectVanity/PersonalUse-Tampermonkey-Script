cd176fc36740aee1322d26797348c700f4f8c414
何意挽秋風 <94283631+RejectVanity@users.noreply.github.com>
2023-10-11 22:02:10 +0800

    Update Source-code.js
    增加对zh-hant的替换

--- Wikipedia-Redirect-zh-tw/Source-code.js
+++ Wikipedia-Redirect-zh-tw/Source-code.js
@@ -5,19 +5,20 @@
 // @description  重定向 zh.m.wikipedia.org/zh-cn/ 到 zh.m.wikipedia.org/zh-tw/
 // @author       您
 // @match        https://zh.m.wikipedia.org/zh-cn/*
+// @match        https://zh.m.wikipedia.org/zh-hant/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // 獲取當前地址欄的 URL
+    // 获取当前地址栏的 URL
     var currentURL = window.location.href;
 
-    // 檢查地址欄是否包含 "https://zh.m.wikipedia.org/zh-cn/"
-    if (currentURL.includes("https://zh.m.wikipedia.org/zh-cn/")) {
-        // 如果是，重定向到對應的地址，保留頁面路徑
-        var newPath = currentURL.replace("https://zh.m.wikipedia.org/zh-cn/", "https://zh.m.wikipedia.org/zh-tw/");
+    // 检查地址栏是否包含 "https://zh.m.wikipedia.org/zh-cn/" 或 "https://zh.m.wikipedia.org/zh-hant/"
+    if (currentURL.includes("https://zh.m.wikipedia.org/zh-cn/") || currentURL.includes("https://zh.m.wikipedia.org/zh-hant/")) {
+        // 如果是，重定向到对应的地址，保留页面路径
+        var newPath = currentURL.replace("/zh-cn/", "/zh-tw/").replace("/zh-hant/", "/zh-tw/");
         window.location.href = newPath;
     }
 })();
